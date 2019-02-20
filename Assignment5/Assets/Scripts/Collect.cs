6cde0c09179c7008b0b6ce8cae10c8f48226a056
Kaio <kaiobarb@gmail.com>
2019-02-19 23:35:32 -0800

    upload those file bb

---
+++ Assignment5/Assets/Scripts/Collect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+    private Animator gateAnimator;
+
+    private bool hasKey;
+
+    void Start()
+    {
+        hasKey = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectible"))
+        {
+            other.gameObject.SetActive(false);
+            particleSystem.transform.position = other.transform.position;
+            particleSystem.Play();
+            hasKey = true;
+        }
+        else if (other.gameObject.CompareTag("Gate")) {
+            Debug.Log(hasKey);
+            gateAnimator = other.GetComponent<Animator>();
+            if (hasKey)
+            {
+                gateAnimator.Play("GateOpen", 0, 0);
+            }
+            
+        }
+        Debug.Log("Collided");
+    }
+}
---
+++ Assignment5/Assets/Scripts/Interact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Collectible"))
+    //    {
+    //        other.gameObject.SetActive(false);
+    //    }
+    //    Debug.Log("Collided");
+    //}
+}
